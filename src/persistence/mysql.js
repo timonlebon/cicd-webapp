ee83f41e545e07d031c19aff032898ab0c6d31cf
Timon Lebon <126574113+timonlebon@users.noreply.github.com>
2024-10-20 19:21:54 +0200

    Update mysql.js

--- src/persistence/mysql.js
+++ src/persistence/mysql.js
@@ -10,7 +10,9 @@ export async function initialize() {
     connection: process.env.MYSQL_URL,
   });
   await knexInstance.raw('CREATE DATABASE IF NOT EXISTS `animals`;');
-  await knexInstance.raw('CREATE TABLE IF NOT EXISTS `animals` (`id` INT NOT NULL, `name` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));');
+  await knexInstance.raw(
+    'CREATE TABLE IF NOT EXISTS `animals` (`id` INT NOT NULL, `name` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));'
+  );
 
   const [animalCount] = await knexInstance('animals').count();
 
